a0e27e8cd3c2aac0aeedb99e7b08ee6e7e7e0151
Karay0524 <5585@holbertonstudents.com>
2022-09-28 12:44:49 -0700

    readme

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *
+ * Description: Prints the alphabet with _putchar
+ * @c: character
+ * Return:void
+ */
+
+int _isupper(int c)
+{
+  if (c >= 65 && c <= 90)
+    {
+      return (1);
+    }
+  return (0);
+}
